935deb15c0bd605557eac05b06110d14ed7c38f5
VinaySavla <savlavinay77@gmail.com>
2025-09-13 20:34:29 +0530

    mongo connection troubleshoot

--- api/discord.js
+++ api/discord.js
@@ -19,7 +19,6 @@ const connectToDatabase = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       bufferCommands: false,
-      bufferMaxEntries: 0,
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
     });
